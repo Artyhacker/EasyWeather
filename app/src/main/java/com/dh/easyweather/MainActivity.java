08db436319452fab7275de6cf26f0f10cea242b9
artyhacker <artyhacker@outlook.com>
2016-11-04 21:56:33 +0800

    today,tomorrow and the after weather

--- app/src/main/java/com/dh/easyweather/MainActivity.java
+++ app/src/main/java/com/dh/easyweather/MainActivity.java
@@ -2,8 +2,6 @@ package com.dh.easyweather;
 
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,7 +9,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -21,7 +18,6 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 
@@ -32,17 +28,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private TextView cityName;
     private TextView weatherText;
     private TextView temperature;
-    private TextView lastUpdate;
     private Button receiveBtn;
     private EditText cityNameEdit;
+
+    private TextView todayWeather;
+    private TextView todayTemperature;
+    private TextView tomorrowWeather;
+    private TextView tomorrowTemperature;
+    private TextView afterTomorrowWeather;
+    private TextView afterTomorrowTemperature;
 
     private Context mContext;
     private ProgressDialog progressDialog;
 
-    String cityNameString = "";
-    String weatherTextString = "";
-    String temperatureString = "";
-    String lastUpdateString = "";
+    private String cityNameString = "";
+    private String weatherTextString = "";
+    private String temperatureString = "";
+    private String todayDayWeatherString = "";
+    private String todayNightWeatherString = "";
+    private String todayTemperatureHigh = "";
+    private String todayTemperatureLow = "";
+    private String tomorrowDayWeatherString = "";
+    private String tomorrowNightWeatherString = "";
+    private String tomorrowTemperatureHigh = "";
+    private String tomorrowTemperatureLow = "";
+    private String afterTomorrowDayWeatherString = "";
+    private String afterTomorrowNightWeatherString = "";
+    private String afterTomorrowTemperatureHigh = "";
+    private String afterTomorrowTemperatureLow = "";
+
+    private String response = "";
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,74 +70,93 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         cityName = (TextView) findViewById(R.id.city_name_id);
         weatherText = (TextView) findViewById(R.id.weather_text_id);
         temperature = (TextView) findViewById(R.id.temperature_id);
-        lastUpdate = (TextView) findViewById(R.id.update_time_id);
         cityNameEdit = (EditText) findViewById(R.id.city_name_edit);
         receiveBtn = (Button) findViewById(R.id.receive_btn);
+        todayWeather = (TextView) findViewById(R.id.today_weather_text);
+        todayTemperature = (TextView) findViewById(R.id.today_temperature);
+        tomorrowWeather = (TextView) findViewById(R.id.tomorrow_weather_text);
+        tomorrowTemperature = (TextView) findViewById(R.id.tomorrow_temperature);
+        afterTomorrowWeather = (TextView) findViewById(R.id.aftertomorrow_weather_text);
+        afterTomorrowTemperature = (TextView) findViewById(R.id.aftertomorrow_temperature);
+
         receiveBtn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         if(view.getId() == R.id.receive_btn){
-            /*if(cityNameEdit.getText().toString().isEmpty()){
-                Toast.makeText(mContext,"请输入城市名！",Toast.LENGTH_SHORT).show();
-                return;
-            }*/
-            String urlString = "";
+            String urlNowString = "";
+            String urlDailyString = "";
             try {
                 if(!cityNameEdit.getText().toString().isEmpty()) {
-                    urlString = "https://api.thinkpage.cn/v3/weather/now.json?key="
+                    urlNowString = "https://api.thinkpage.cn/v3/weather/now.json?key="
                             + KEY + "&location="
                             + URLEncoder.encode(cityNameEdit.getText().toString(), "UTF-8")
                             + "&language=zh-Hans&unit=c";
+                    urlDailyString = "https://api.thinkpage.cn/v3/weather/daily.json?key="
+                            + KEY + "&location="
+                            + URLEncoder.encode(cityNameEdit.getText().toString(), "UTF-8")
+                            + "&language=zh-Hans&unit=c&start=0&days=3";
                 } else {
-                    urlString = "https://api.thinkpage.cn/v3/weather/now.json?key="
+                    urlNowString = "https://api.thinkpage.cn/v3/weather/now.json?key="
                             + KEY + "&location="
                             + "ip"
                             + "&language=zh-Hans&unit=c";
+                    urlDailyString = "https://api.thinkpage.cn/v3/weather/daily.json?key="
+                            + KEY + "&location="
+                            + "ip"
+                            + "&language=zh-Hans&unit=c&start=0&days=3";
                 }
             } catch (Exception e){
                 e.printStackTrace();
             }
             showProgressDialog();
-            sendHttpRequest(urlString);
+            sendHttpRequest(urlNowString, urlDailyString);
         }
     }
 
-    private void sendHttpRequest(final String urlString) {
-
+    private void sendHttpRequest(final String urlNowString, final String urlDailyString) {
         new Thread(new Runnable() {
-            HttpURLConnection connection = null;
             @Override
             public void run() {
-                try {
-                    URL url = new URL(urlString);
-                    connection = (HttpURLConnection) url.openConnection();
-                    connection.setRequestMethod("GET");
-                    connection.setConnectTimeout(8000);
-                    connection.setReadTimeout(8000);
-                    InputStream in = connection.getInputStream();
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-                    StringBuilder builder = new StringBuilder();
-                    String line;
-                    while((line = reader.readLine())!=null){
-                        builder.append(line);
-                    }
-                    String response = builder.toString();
-                    //Log.d("MainActivity", response);
-                    parseJSON(response);
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }finally {
-                    if(connection != null)
-                        connection.disconnect();
-                }
+                String responseNow = urlToString(urlNowString);
+                parseNowJSON(responseNow);
+                String responseDaily = urlToString(urlDailyString);
+                //Log.d("MainActivity", responseDaily);
+                parseDailyJSON(responseDaily);
             }
         }).start();
     }
 
-    private void parseJSON(String response) {
+
+
+    private String urlToString(String urlString){
+        HttpURLConnection connection = null;
+        String response = "";
+        try {
+            URL url = new URL(urlString);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(8000);
+            connection.setReadTimeout(8000);
+            InputStream in = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder builder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+            }
+            response = builder.toString();
+        } catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            if(connection != null)
+                connection.disconnect();
+            return response;
+        }
+    }
+
+    private void parseNowJSON(String response) {
         try {
             JSONObject jsonObject = new JSONObject(response);
             JSONObject results = jsonObject.getJSONArray("results").getJSONObject(0);
@@ -131,7 +167,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             cityNameString = location.getString("name");
             weatherTextString = now.getString("text");
             temperatureString = now.getString("temperature");
-            lastUpdateString = results.getString("last_update");
             //Log.d("MainActivity",cityNameString + "---" + weatherTextString + "---"
                     //+ temperatureString + "---" + lastUpdateString);
 
@@ -139,9 +174,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 @Override
                 public void run() {
                     cityName.setText("城市： " + cityNameString);
-                    weatherText.setText("天气： " + weatherTextString);
-                    temperature.setText("温度： " + temperatureString + "度");
-                    lastUpdate.setText("上次更新： " + lastUpdateString);
+                    weatherText.setText("实时天气： " + weatherTextString);
+                    temperature.setText("实时温度： " + temperatureString + "度");
+                    //lastUpdate.setText("上次更新： " + lastUpdateString);
                     closeProgressDialog();
                 }
             });
@@ -151,6 +186,71 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    private void parseDailyJSON(String responseDaily) {
+        try {
+            JSONObject jsonObject = new JSONObject(responseDaily);
+            JSONObject results = jsonObject.getJSONArray("results").getJSONObject(0);
+            //Log.d("MainActivity", results.toString());
+            JSONArray daily = results.getJSONArray("daily");
+            //Log.d("MainActivity", daily.toString());
+            JSONObject today = daily.getJSONObject(0);
+            JSONObject tomorrow = daily.getJSONObject(1);
+            JSONObject afterTomorrow = daily.getJSONObject(2);
+            //Log.d("MainActivity","today: " + today.toString() + "--- tomorrow: "
+            //        + tomorrow.toString() + "--- afterTomorrow: " + afterTomorrow.toString());
+            todayDayWeatherString = today.getString("text_day");
+            todayNightWeatherString = today.getString("text_night");
+            todayTemperatureHigh = today.getString("high");
+            todayTemperatureLow = today.getString("low");
+            /*Log.d("MainActivity", "今天的天气是" + todayDayWeatherString + " 转 "
+                    + todayNightWeatherString + "; 今天的气温是 " + todayTemperatureLow
+                    + "度 - " + todayTemperatureHigh + "度");*/
+            tomorrowDayWeatherString = tomorrow.getString("text_day");
+            tomorrowNightWeatherString = tomorrow.getString("text_night");
+            tomorrowTemperatureHigh = tomorrow.getString("high");
+            tomorrowTemperatureLow = tomorrow.getString("low");
+            afterTomorrowDayWeatherString = afterTomorrow.getString("text_day");
+            afterTomorrowNightWeatherString = afterTomorrow.getString("text_night");
+            afterTomorrowTemperatureHigh = afterTomorrow.getString("high");
+            afterTomorrowTemperatureLow = afterTomorrow.getString("low");
+            Log.d("MainActivity", "明天的天气是" + tomorrowDayWeatherString + " 转 "
+                    + tomorrowNightWeatherString + "; 明天的气温是 " + tomorrowTemperatureLow
+                    + "度 - " + tomorrowTemperatureHigh + "度");
+            Log.d("MainActivity", "后天的天气是" + afterTomorrowDayWeatherString + " 转 "
+                    + afterTomorrowNightWeatherString + "; 后天的气温是 " + afterTomorrowTemperatureLow
+                    + "度 - " + afterTomorrowTemperatureHigh + "度");
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(!todayDayWeatherString.equals(todayNightWeatherString)) {
+                        todayWeather.setText("今天天气：" + todayDayWeatherString + " 转 "
+                                + todayNightWeatherString);
+                    } else
+                        todayWeather.setText("今天天气：" + todayDayWeatherString);
+                    todayTemperature.setText("今天气温：" + todayTemperatureLow + "度-"
+                            + todayTemperatureHigh + "度");
+
+                    if(!tomorrowDayWeatherString.equals(tomorrowNightWeatherString)) {
+                        tomorrowWeather.setText("明天天气：" + tomorrowDayWeatherString + " 转 "
+                                + tomorrowNightWeatherString);
+                    } else
+                        tomorrowWeather.setText("明天天气：" + tomorrowDayWeatherString);
+                    tomorrowTemperature.setText("明天气温：" + tomorrowTemperatureLow + "度-"
+                            + tomorrowTemperatureHigh + "度");
+
+                    if(!afterTomorrowDayWeatherString.equals(afterTomorrowNightWeatherString)) {
+                        afterTomorrowWeather.setText("后天天气：" + afterTomorrowDayWeatherString + " 转 "
+                                + afterTomorrowNightWeatherString);
+                    } else
+                        afterTomorrowWeather.setText("后天天气：" + afterTomorrowDayWeatherString);
+                    afterTomorrowTemperature.setText("后天气温：" + afterTomorrowTemperatureLow + "度-"
+                            + afterTomorrowTemperatureHigh + "度");
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     private void showProgressDialog() {
         if(progressDialog == null){
